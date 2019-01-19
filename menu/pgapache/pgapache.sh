61d3aa61bf6d42acfd9d28309e68616c87d76c55
Admin9705 <user@dunn.cloud>
2019-01-19 02:16:18 -0500

    update

---
+++ menu/pgapache/pgapache.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+#
+# Title:      PlexGuide (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://plexguide.com - http://github.plexguide.com
+# GNU:        General Public License v3.0
+################################################################################
+
+# FUNCTIONS START ##############################################################
+source /opt/plexguide/menu/functions/functions.sh
+
+initial () {
+  rolename='pgapache'
+  mkdir -p "/opt/$rolename"
+  ansible-playbook "/opt/plexguide/menu/$rolename/$rolename.yml"
+
+  echo ""
+  echo "💬  Pulling Update Files - Please Wait"
+  file="/opt/pgapache/place.holder"
+  waitvar=0
+  while [ "$waitvar" == "0" ]; do
+  	sleep .5
+  	if [ -e "$file" ]; then waitvar=1; fi
+  done
+}
+
+mainbanner () {
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 PG Apache                          📓 Reference: pgapache.plexguide.com
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+💬 PG Apache is a combined group of services that enables the user to
+deploy their own wordpress websites; including the use of other multiple
+instances!
+
+[1] Utilize PG Apache - PlexGuide's
+[2] Utilize PG Apache - Personal (Forked)
+[Z] Exit
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+read -p 'Type a Selection | Press [ENTER]: ' typed < /dev/tty
+
+case $typed in
+    1 )
+        initial ;;
+    2 )
+        variable /var/plexguide/pgapache.user "NOT-SET"
+        variable /var/plexguide/pgapache.branch "NOT-SET"
+        pinterface ;;
+    z )
+        exit ;;
+    Z )
+        exit ;;
+    * )
+        mainbanner ;;
+esac
+}
+
+pinterface () {
+
+apacheuser=$(cat /var/plexguide/pgapache.user)
+apachebranch=$(cat /var/plexguide/pgapache.branch)
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 PG Apache!                        📓 Reference: pgapache.plexguide.com
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+💬 User: $apacheuser | Branch: $apachebranch
+
+[1] Change User Name & Branch
+[2] Deploy PG Apache - Personal (Forked)
+[Z] Exit
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+read -p 'Type a Selection | Press [ENTER]: ' typed < /dev/tty
+
+case $typed in
+    1 )
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+💬 IMPORTANT MESSAGE
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+Username & Branch are both case sensitive! Normal default branch is v8,
+but check the branch under your fork that is being pulled!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+        read -p 'Username | Press [ENTER]: ' apacheuser < /dev/tty
+        read -p 'Branch   | Press [ENTER]: ' apachebranch < /dev/tty
+        echo "$apacheuser" > /var/plexguide/pgapache.user
+        echo "$apachebranch" > /var/plexguide/pgapache.branch
+        pinterface ;;
+    2 )
+        existcheck=$(git ls-remote --exit-code -h "https://github.com/$apacheuser/PlexGuide-Apache" | grep "$apachebranch")
+        if [ "$existcheck" == "" ]; then echo;
+        read -p '💬 Exiting! Forked Version Does Not Exist! | Press [ENTER]: ' typed < /dev/tty
+        mainbanner; fi
+        initial ;;
+    z )
+        exit ;;
+    Z )
+        exit ;;
+    * )
+        mainbanner ;;
+esac
+}
+
+# FUNCTIONS END ##############################################################
+echo "" > /tmp/output.info
+mainbanner
